cf2d656a64c54916fb38b7c0c9c1bcf8636026ba
Mohamed-Zkaria <mohamed.zakaria_10_7@yahoo.com>
2020-02-23 19:51:03 +0200

    Modifying some CSS and fixing a bug in JS.

--- JS/displayUserOrders.js
+++ JS/displayUserOrders.js
@@ -1,6 +1,7 @@
 let form = document.getElementById("orderForm");
 let inputTotal = document.querySelector("input[name='totalprice']");
 inputTotal.value = 0;
+let indexToStart = 0;
 function addToCart(){
     // window.alert(this.dataset.name + " "  + this.dataset.price + " " + this.dataset.id);
 
@@ -8,38 +9,29 @@ function addToCart(){
         window.alert("Item already in cart");
     } else {
 
-
-        let item = document.createElement("input");
-        item.type="hidden";
-        item.name="order[]";
+        item = createHiddenInput();
         item.value = this.dataset.id + ":" + 1;
         item.title = this.dataset.name;
         item.dataset.name= this.dataset.name + this.dataset.id;
-        
-        
+                
         let label = document.createElement("label");
         label.dataset.name= this.dataset.name + this.dataset.id;
         label.innerHTML = this.dataset.name + ":";
         
-        let counter = document.createElement("input");
-        counter.setAttribute("min", 0);
-        counter.setAttribute("max", 100);
-        counter.setAttribute("step", 1);
+        counter = createNumberInput();
         counter.title = this.dataset.name;
         counter.dataset.name= this.dataset.name + this.dataset.id;
         counter.dataset.price = this.dataset.price;
-        counter.dataset.lastvalue = 1;
-        counter.value = 1;
-        counter.type="number";
 
         counter.addEventListener("change", totalAmountAdded);
         
         // addin the items price to the total amount
         inputTotal.value = ( parseInt(inputTotal.value) +  parseInt(this.dataset.price))  ;
         
-        form.insertBefore(label, form[0]);
-        form.insertBefore(item, form[0]);
-        form.insertBefore(counter, form[0]);
+        form.insertBefore(label, form[indexToStart]);
+        form.insertBefore(item, form[indexToStart]);
+        form.insertBefore(counter, form[indexToStart]);
+        indexToStart++;
     }
     
     // item.checked = "checked";
@@ -48,19 +40,37 @@ function addToCart(){
 }
 
 
+function createHiddenInput(){
+    let item = document.createElement("input");
+    item.type="hidden";
+    item.name="order[]";
+    return item;
+}
+
+function createNumberInput(){
+    let counter = document.createElement("input");
+    counter.setAttribute("min", 0);
+    counter.setAttribute("max", 100);
+    counter.setAttribute("step", 1);
+    counter.type="number";
+    counter.value = 1;
+    counter.dataset.lastvalue = 1;
+    return counter;
+}
+
 
 function totalAmountAdded(){
     let lastvalue = parseInt(this.dataset.lastvalue);
     let currentvalue = parseInt(this.value);
 
-    if(this.value == 0){  // removing the input if it reaches 0 
+    if(this.value >= 0){  // removing the input if it reaches 0 
         let parent = this.parentElement;
         inputTotal.value = ( parseInt(inputTotal.value ) - ( parseInt(this.dataset.price) * (lastvalue - currentvalue) ) );
         console.log(this.dataset.name); 
         parent.removeChild(document.querySelector("input[data-name='" + this.dataset.name + "']"));
         parent.removeChild(document.querySelector("input[data-name='" + this.dataset.name + "']"));
         parent.removeChild(document.querySelector("label[data-name='" + this.dataset.name + "']"));
-        
+        indexToStart--;
     } else {
         
         
